49067ec20909243bdc100a4c4e743c5c90c5927f
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-17 19:23:27 -0300

    Estudo c# condicional IF

---
+++ Condicional-IF/Condicional-IF/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Cliente Especial (S/N)");
+var cliente = Console.ReadLine();
+
+if(cliente == "S" || cliente == "s")
+{
+    Console.WriteLine("\nCliente especial");
+}
+else
+{
+    Console.WriteLine("\nCliente não especial");
+}
