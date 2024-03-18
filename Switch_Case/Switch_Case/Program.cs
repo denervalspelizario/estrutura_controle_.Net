f9dd464e782b0af84d4408c274685c17d8170f31
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-03-18 19:38:18 -0300

    estudo c# uso do switch case, while e for no c#

---
+++ Switch_Case/Switch_Case/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("SWITCH CASE NO C#");
+
+
+int valor = 300;
+
+
+
+Console.WriteLine("Deseja parcelar em quantas prestações (Max em 3x)");
+int prestacao = Convert.ToInt32(Console.ReadLine());
+
+
+switch(prestacao)
+{
+    case 1: // se for 1
+        Console.WriteLine("Prestação no valor de 300");
+       break;
+
+    case 2: // se for 2
+        Console.WriteLine("Prestação no valor de 150");
+        break;
+    case 3: // se for 3
+        Console.WriteLine("Prestação no valor de 100");
+        break;
+
+    default: // caso o valor seja diferentes dos 1 , 2 e 3
+        Console.WriteLine("Valor digitado inválido");
+        break;
+    
+
+}
+    
---
+++ While_For/While_For/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("## uso de While e For");
+
+
+Console.WriteLine("Escolha um numero de para fazer a tabuada entre 1 a 9");
+int numeroTabuada = Convert.ToInt32(Console.ReadLine());
+
+if(numeroTabuada >= 10 || numeroTabuada <= 0 )
+{
+    Console.WriteLine("Numero inválido, somente entre 1 a 9");
+}
+
+int multiplicador = 1;
+
+while(multiplicador < 11)
+{
+    Console.WriteLine($"{numeroTabuada} * {multiplicador} = {numeroTabuada * multiplicador} \n");
+    multiplicador++;
+}
+
+Console.WriteLine("=============================\n");
+Console.WriteLine("Uso do for");
+
+Console.WriteLine("Escolha um numero de para fazer a tabuada entre 1 a 9");
+int numeroTabuadaFor = Convert.ToInt32(Console.ReadLine());
+
+if (numeroTabuadaFor >= 10 || numeroTabuadaFor <= 0)
+{
+    Console.WriteLine("Numero inválido, somente entre 1 a 9");
+}
+
+int multiplicadorFor = 1;
+
+for(int i = 0; multiplicadorFor < 11; multiplicadorFor++) // enquanto o multiplicadorFor for menor que 11
+{
+    Console.WriteLine($"{numeroTabuadaFor} * {multiplicadorFor} = {numeroTabuadaFor * multiplicadorFor} \n");
+}
